819abd4fe34c150cacb3151a1f38aa2aab132c74
XmasterFire <trevroeder@gmail.com>
2014-09-22 18:55:22 -0400

    battleship

---
+++ battilship.py
@@ -0,0 +1,22 @@
+print("hello the world")
+class Ship(): pass
+
+class Carrier (Ship):
+    size = 5
+
+class Ptboat (Ship):
+    size = 2
+
+class Submarine(Ship):
+    size = 3
+class Battleship(Ship):
+    size = 4
+class Destroyer (Ship):
+    size = 3
+
+
+w = '~'
+
+
+row = [w,w,w,w,w,w,w,w,w,w,w,w,w,w,w]
+print(row)
